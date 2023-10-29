b8a1b6d29e0dc7ac3cc99c7ad09957cd68f7922f
rmf2pt <rmf2pt@gmail.com>
2023-10-29 14:09:18 +0000

    Add canvas and input with logic to change canvas color
    Signed-off-by: rmf2pt <rmf2pt@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
+import { ChangeEvent, useState } from "react";
+
 function App() {
+  const [color, setColor] = useState<string>("Empty value");
+
+  const handleColorChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setColor(e.target.value);
+  };
+
   return (
     <>
-      <h1>React Color Picking</h1>
+      <div className="canvas" style={{ backgroundColor: color }}>
+        <p>{!color ? "Empty value" : color}</p>
+      </div>
+      <input
+        type="text"
+        placeholder="Add color name"
+        autoFocus
+        value={color === "Empty value" ? "" : color}
+        onChange={handleColorChange}
+      />
     </>
   );
 }
