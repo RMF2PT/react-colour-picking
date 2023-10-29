99b5817d85240fe329229c6b9a2fedf390e30eef
rmf2pt <rmf2pt@gmail.com>
2023-10-29 15:50:55 +0000

    Add Hex values and automatic change of font color is brightness of canvas is low
    Signed-off-by: rmf2pt <rmf2pt@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
 import { ChangeEvent, useState } from "react";
+import colorNames from "colornames";
 import Canvas from "./components/Canvas";
 import InputColor from "./components/InputColor";
 
 function App() {
   const [color, setColor] = useState<string>("Empty value");
+  const [hexColor, setHexColor] = useState<string>("");
 
   const handleColorChange = (e: ChangeEvent<HTMLInputElement>) => {
     setColor(e.target.value);
+    setHexColor(colorNames(e.target.value) as string);
   };
 
   return (
     <>
-      <Canvas color={color} />
+      <Canvas color={color} hexColor={hexColor} />
       <InputColor color={color} handleColorChange={handleColorChange} />
     </>
   );
--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -1,11 +1,27 @@
 type CanvasProps = {
   color: string;
+  hexColor: string;
 };
 
-const Canvas = ({ color }: CanvasProps) => {
+const Canvas = ({ color, hexColor }: CanvasProps) => {
+  const isDarkColor = (hexColor: string) => {
+    if (!hexColor) return false;
+    const hex = hexColor.replace("#", "");
+    const c_r = parseInt(hex.substring(0, 2), 16);
+    const c_g = parseInt(hex.substring(2, 4), 16);
+    const c_b = parseInt(hex.substring(4, 6), 16);
+    const brightness = (c_r * 299 + c_g * 587 + c_b * 114) / 1000;
+    return brightness < 155;
+  };
+
   return (
     <div className="canvas" style={{ backgroundColor: color }}>
-      <p>{!color ? "Empty value" : color}</p>
+      <p className={`${isDarkColor(hexColor as string) ? "light" : "dark"}`}>
+        {color ? color : "Empty value"}
+      </p>
+      <p className={`${isDarkColor(hexColor as string) ? "light" : "dark"}`}>
+        {hexColor ? hexColor : null}
+      </p>
     </div>
   );
 };
